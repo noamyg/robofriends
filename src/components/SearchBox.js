66323701b83e74e4883078b66ebb163d99bcfd97
admin@noamyg.com <noamyg@gmail.com>
2018-11-25 20:32:34 +0200

    Accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,11 +3,14 @@ import React from 'react'
 const SearchBox = ({ searchField, searchChange }) => {
 	return (
 		<div className="pa2 ba b--green bg-lightest-blue">
-			<input
-				type="search"
-				placeholder="Search Robot"
-				onChange={searchChange}
-			/>	
+			<label for="search">Search Robots
+				<input
+					id="robot_search"
+					type="search"
+					placeholder="Search Robot"
+					onChange={searchChange}
+				/>	
+			</label>
 		</div>
 		
 	)
