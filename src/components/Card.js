eff71af1fe0befa52792d203776d1af667e0adf4
admin@noamyg.com <noamyg@gmail.com>
2018-11-27 08:33:33 +0200

    Robot Sizes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({robot}) => {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw3 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${robot.id}?200x200`}/>
+			<img alt='robot' src={`https://robohash.org/${robot.id}?size=200x200`}/>
 			<div>
 				<h2>{robot.name}</h2>
 				<p>{robot.email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,14 +3,13 @@ import React from 'react'
 const SearchBox = ({ searchField, searchChange }) => {
 	return (
 		<div className="pa2 ba b--green bg-lightest-blue">
-			<label for="search">
-				<input
-					id="robot_search"
-					type="search"
-					placeholder="Search Robot"
-					onChange={searchChange}
-				/>	
-			</label>
+			<input
+				aria-label="Search Box"
+				id="robot_search"
+				type="search"
+				placeholder="Search Robot"
+				onChange={searchChange}
+			/>
 		</div>
 		
 	)
